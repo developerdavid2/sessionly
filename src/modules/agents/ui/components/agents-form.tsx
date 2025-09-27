6f3946f584e2b419836f9827d2c065120536d1de
Jacobs David <jacobsdavid.dr@gmail.com>
2025-09-27 16:57:31 +0100

    13: agents filters

--- src/modules/agents/ui/components/agents-form.tsx
+++ src/modules/agents/ui/components/agents-form.tsx
@@ -36,7 +36,7 @@ const AgentsForm = ({
   const createAgent = useMutation(
     trpc.agents.create.mutationOptions({
       onSuccess: () => {
-        queryClient.invalidateQueries(trpc.agents.getMany.queryOptions());
+        queryClient.invalidateQueries(trpc.agents.getMany.queryOptions({}));
 
         if (initialValues?.id) {
           queryClient.invalidateQueries(
--- src/modules/agents/ui/views/agents-view.tsx
+++ src/modules/agents/ui/views/agents-view.tsx
@@ -8,15 +8,26 @@ import ErrorState from "@/components/error-state";
 import { columns } from "@/modules/agents/ui/components/columns";
 import { DataTable } from "@/modules/agents/ui/components/data-table";
 import EmptyState from "@/components/empty-state";
+import { useAgentsFilters } from "@/modules/agents/hooks/use-agents-filters";
+import DataPagination from "@/modules/agents/ui/components/data-pagination";
 
 export const AgentsView = () => {
+  const [filters, setFilters] = useAgentsFilters();
+
   const trpc = useTRPC();
-  const { data } = useSuspenseQuery(trpc.agents.getMany.queryOptions());
+  const { data } = useSuspenseQuery(
+    trpc.agents.getMany.queryOptions({ ...filters }),
+  );
 
   return (
     <div className="flex-1 pb-4 px-4 md:px-8 flex flex-col gap-y-4">
-      <DataTable columns={columns} data={data} />
-      {data.length === 0 && (
+      <DataTable columns={columns} data={data.items} />
+      <DataPagination
+        page={filters.page}
+        totalPages={data.totalPages}
+        onPageChange={(page) => setFilters({ page })}
+      />
+      {data.items.length === 0 && (
         <EmptyState
           title="Create your first agent"
           description="Get started by creating your first agent. Agents can help you automate tasks and improve productivity."
